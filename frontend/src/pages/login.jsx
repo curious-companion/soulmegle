885c1eafe85836acbad60d37f99205b830db3c15
Vibhanshu Jain <105918194+curious-companion@users.noreply.github.com>
2025-03-01 12:52:25 +0530

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
 import { Link , useNavigate } from "react-router-dom";
-import { useAuth } from "../context/authContext";
+import { useAuth } from "../context/AuthContext";
 import "../styles/auth.css"; 
 import ParticlesBackground from "../components/ParticlesBackground";
 import { FcGoogle } from "react-icons/fc";
